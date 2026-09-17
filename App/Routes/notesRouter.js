645a1970326a2f67785c39795198027f00cf062e
Kelvin Addy <Keladdy2131@gmail.com>
2026-09-17 23:31:32 +0000

    Fixed missing colon causing unknown error handler to trigger in put router path

--- App/Routes/notesRouter.js
+++ App/Routes/notesRouter.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 router.get('/', notesController.getAllNotes);
 router.get('/:id', notesController.getNoteById);
-router.put('/id', notesController.putNoteById);
+router.put('/:id', notesController.putNoteById);
 router.delete('/:id', notesController.deleteNoteById);
 router.post('/', notesController.postNote);
 
